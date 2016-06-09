662347fab0e2a0a3c428c6ce55a67603387ed5cf
Jeremy Felt <jeremy.felt@gmail.com>
2016-06-09 14:46:06 -0700

    Only display monthly archive links from March 2016 (#2)

--- functions.php
+++ functions.php
@@ -1,1 +1,7 @@
 <?php
+
+add_filter( 'getarchives_where', 'wsu_president_filter_archives' );
+
+function wsu_president_filter_archives( $sql_where ) {
+	return $sql_where . " AND post_date > '2016-03-27 00:00:00' ";
+}
