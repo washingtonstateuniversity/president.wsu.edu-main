9a335edb4597367335ef124d512ecedebe279710
Jeremy Felt <jeremy.felt@gmail.com>
2017-04-14 15:12:55 -0700

    Extend the length of a public post preview URL to 5 days

--- functions.php
+++ functions.php
@@ -5,3 +5,13 @@ add_filter( 'getarchives_where', 'wsu_president_filter_archives' );
 function wsu_president_filter_archives( $sql_where ) {
 	return $sql_where . " AND post_date > '2016-03-27 00:00:00' ";
 }
+
+add_filter( 'ppp_nonce_life', 'wsu_president_filter_ppp_nonce_life' );
+/**
+ * Extends the length of a public post preview URL.
+ *
+ * @return int
+ */
+function wsu_president_filter_ppp_nonce_life() {
+	return 60 * 60 * 24 * 5;
+}
